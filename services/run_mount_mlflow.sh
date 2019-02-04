c23fc150fdcf4255c1329e2a454f0586d8ec131d
Vladimir Shulyak <vladimir@shulyak.net>
2019-02-04 19:52:44 +0300

    Remove agressive caching for mlflow mount

--- services/run_mount_mlflow.sh
+++ services/run_mount_mlflow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 uid=`id -u jupyter`
 gid=`id -g jupyter`
-goofys -f -o allow_other --uid $uid --gid $gid --region $MLFLOW_DATA_BUCKET_REGION $MLFLOW_DATA_BUCKET:$MLFLOW_DATA_BUCKET_PREFIX $MLFLOW_DATA_BUCKET_MOUNT
+goofys -f -o allow_other --uid $uid --gid $gid --stat-cache-ttl 400ms --type-cache-ttl 100ms --region $MLFLOW_DATA_BUCKET_REGION $MLFLOW_DATA_BUCKET:$MLFLOW_DATA_BUCKET_PREFIX $MLFLOW_DATA_BUCKET_MOUNT
